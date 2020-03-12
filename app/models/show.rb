e795e973773fc880762057bc379930661355a3ec
dwane richards <dwanerich@gmail.com>
2020-03-12 01:08:41 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base
   
-  def self.highest_rating
-  Show.maximum(:rating)
+def Show::highest_rating
+    Show.maximum("rating")
   end
-  
-  def self.most_popular_show
-    Show.all.where("rating = ?", 9)
+
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
   end
-  
-  def self.lowest_rating
-    Show.minimum(:rating)
+
+  def Show::lowest_rating
+    Show.minimum("rating")
   end
-  
-  def self.least_popular_show
-    Show.where("rating = ?", 1)
+
+  def Show::least_popular_show
+    Show.order(rating: :asc).first
   end
-  
-  def self.ratings_sum
-    Show.all(:rating).count
+
+  def Show::ratings_sum
+    Show.sum("rating")
   end
-  
-  def self.popular_shows
-    Show.where("rating > ?", 5)
+
+  def Show::popular_shows
+    Show.where("rating > 5")
   end
-  
-  def self.shows_by_alphabetical_order
-    Show.all.sort
+
+  def Show::shows_by_alphabetical_order
+    Show.order(name: :asc)
   end
 end
